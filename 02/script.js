2a9f757c8a2a297272a235b92304ce322ec0c006
Nerd It Yourself <nerdityourself.firenze@gmail.com>
2021-07-28 10:13:41 +0200

    Fix example

--- 02/script.js
+++ 02/script.js
@@ -8,11 +8,14 @@ function example()
 example();
 
 var color = "blue";
+
 function changeColor()
 {
-    var color = "red";
-    document.querySelector(".first-p").style.color = color;
+    color = "green";
+    document.querySelector("#first-p").style.color = color;
 }
 
 changeColor();
-document.querySelector(".second-p").style.color = color;
+
+document.querySelector("#second-p").style.color = color;
+
